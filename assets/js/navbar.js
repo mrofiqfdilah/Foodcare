27eebb66d45399f0512a36c8dcec41b15b379b38
mrofiqfdilah <mrofiqfadil@gmail.com>
2025-10-25 14:04:05 +0700

    Feat(Navbar) : Improve Navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,16 +1,15 @@
+
 const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
 const mobileMenu = document.getElementById('mobile-menu');
-let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
-    menuOpen = !menuOpen;
-    if (menuOpen) {
-        mobileMenu.classList.remove('opacity-0', 'scale-y-0');
-        mobileMenu.classList.add('opacity-100', 'scale-y-100');
-        menuBtn.innerHTML = '<i class="ri-close-line"></i>';
-    } else {
-        mobileMenu.classList.add('opacity-0', 'scale-y-0');
-        mobileMenu.classList.remove('opacity-100', 'scale-y-100');
-        menuBtn.innerHTML = '<i class="ri-menu-line"></i>';
-    }
+    mobileMenu.classList.remove('opacity-0', 'scale-y-0');
+    mobileMenu.classList.add('opacity-100', 'scale-y-100');
 });
+
+closeBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('opacity-0', 'scale-y-0');
+    mobileMenu.classList.remove('opacity-100', 'scale-y-100');
+});
+
