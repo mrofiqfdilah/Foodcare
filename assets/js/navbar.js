d44fb5a4ad71c101e174e04bd3f9a2de5e400403
mrofiqfdilah <mrofiqfadil@gmail.com>
2025-10-25 15:01:01 +0700

    Feat(Home) : Improve Donation Box

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,15 +1,13 @@
-
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 
 menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.remove('opacity-0', 'scale-y-0');
-    mobileMenu.classList.add('opacity-100', 'scale-y-100');
+    mobileMenu.classList.remove('opacity-0', 'scale-y-0', 'pointer-events-none');
+    mobileMenu.classList.add('opacity-100', 'scale-y-100', 'pointer-events-auto');
 });
 
 closeBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('opacity-0', 'scale-y-0');
-    mobileMenu.classList.remove('opacity-100', 'scale-y-100');
+    mobileMenu.classList.add('opacity-0', 'scale-y-0', 'pointer-events-none');
+    mobileMenu.classList.remove('opacity-100', 'scale-y-100', 'pointer-events-auto');
 });
-
